3b3833ba25141309c6ee13f342d74c400ac7df42
Trung Pham <trung@phamcom.com>
2014-05-25 14:01:00 -0700

    support inline js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def inline_js(&block)
+    output = capture(&block)[31..-10]
+
+    signature = Digest::MD5.hexdigest(output)
+
+    content_for :js, "if(!window.js_#{signature}){window.js_#{signature}=true;#{output}}".html_safe
+  end
 end
